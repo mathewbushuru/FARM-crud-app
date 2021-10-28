50f050947935bcb174eda596c0cefda7581d401e
Mathew Bushuru <bushuruwafulamathew@gmail.com>
2021-10-27 19:49:34 -0700

    created visual UI of frontend usin bootstrap for styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App list-group-item justify-content-center align-items-center mx-auto" style={{"width":"400px","backgroundColor":"white","marginTop":"15px"}}>
+
+        <h1 className = "card text-white bg-primary mb-1" styleName="maxWidth:20rem;">Task Manager</h1>
+
+        <h6 className = "card text-white bg-primary mb-3"> FastAPI - React - MongoDB</h6>
+
+        <div className="card-body">
+          <h5 className="card text-white bg-dark mb-3">Add your task</h5>
+          <span className="card-text">
+            <input className="mb-2 form-control titleIn" placeholder='Title' />
+            <input className="mb-2 form-control desIn" placeholder='Description' />
+            <button className="btn btn-outline-primary mx-2 mb-2" style={{'borderRadius':'50px',"font-weight":"bold"}}> Add Task</button>
+          </span>
+
+          <h5 className="card text-white bg-dark mb-3">Your tasks</h5>
+          <div>
+            {/* Todo items - External Components */}
+          </div>
+          <h6 className = "card text-dark bg-warning py-1 mb-0"> Copyright 2021, All rights reserved &copy;</h6>
+        </div>
+
     </div>
   );
 }
